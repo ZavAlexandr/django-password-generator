11aef5e5525a90b4267a4d655ce71a6e39ece63e
ZavAlexandr <zavalex.srv@gmail.com>
2022-04-07 21:39:24 +0300

    Settings

--- password_generator/settings.py
+++ password_generator/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8))n!2ra_gs_cq#7wh2!(pkko(i-60m^#!ka#0ix)h=!ux_cwj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['zavalex.pythonanywhere.com']
 
 
 # Application definition
